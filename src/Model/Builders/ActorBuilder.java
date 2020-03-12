d6426be880880b2fd0a35ba96a5d04e273d212e1
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2020-03-12 17:10:19 +0100

    Add builders

---
+++ src/Model/Builders/ActorBuilder.java
@@ -0,0 +1,71 @@
+package Model.Builders;
+
+import Model.Actor;
+import Model.Media;
+import Model.Sex;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActorBuilder {
+
+    private String name;
+    private LocalDate born;
+    private String biography;
+    private Sex sex;
+    private List<Media> filmography;
+
+    public ActorBuilder() {
+        this.filmography = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBorn() {
+        return born;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public List<Media> getFilmography() {
+        return filmography;
+    }
+
+    public ActorBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public ActorBuilder setBorn(LocalDate born) {
+        this.born = born;
+        return this;
+    }
+
+    public ActorBuilder setBiography(String biography) {
+        this.biography = biography;
+        return this;
+    }
+
+    public ActorBuilder setSex(Sex sex) {
+        this.sex = sex;
+        return this;
+    }
+
+    public ActorBuilder setFilmography(List<Media> filmography) {
+        this.filmography = filmography;
+        return this;
+    }
+
+    public Actor getActor(){
+        return new Actor(this);
+    }
+}
---
+++ src/Model/Builders/MediaBuilder.java
@@ -0,0 +1,116 @@
+package Model.Builders;
+
+import Model.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MediaBuilder {
+
+    protected BigDecimal id;
+    protected String title;
+    protected String description;
+    protected LocalDate premier;
+    protected List<Review> reviews;
+    protected List<Actor> cast;
+
+    public MediaBuilder() {
+        this.reviews = new ArrayList<>();
+        this.cast = new ArrayList<>();
+    }
+
+    public Media getMedia() {
+        Media media = new Media(this.id, this.title, this.description, this.premier);
+        for (Review review : this.reviews) {
+            media.addReview(review);
+        }
+        for (Actor actor : this.cast) {
+            media.addCast(actor);
+        }
+        return media;
+    }
+
+    public Series getSeries() {
+        Series series = new Series(this.id, this.title, this.description, this.premier);
+        for (Review review : this.reviews) {
+            series.addReview(review);
+        }
+        for (Actor actor : this.cast) {
+            series.addCast(actor);
+        }
+        return series;
+    }
+
+    public Movie getMovie() {
+        Movie movie = new Movie(this.id, this.title, this.description, this.premier);
+        for (Review review : this.reviews) {
+            movie.addReview(review);
+        }
+        for (Actor actor : this.cast) {
+            movie.addCast(actor);
+        }
+        return movie;
+    }
+
+    public BigDecimal getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getPremier() {
+        return premier;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public List<Actor> getCast() {
+        return cast;
+    }
+
+    public MediaBuilder setId(BigDecimal id) {
+        this.id = id;
+        return this;
+    }
+
+    public MediaBuilder setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public MediaBuilder setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public MediaBuilder setPremier(LocalDate premier) {
+        this.premier = premier;
+        return this;
+    }
+
+    public MediaBuilder setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+        return this;
+    }
+
+    public MediaBuilder addReview(Review review){
+        this.reviews.add(review);
+        return this;
+    }
+
+    public MediaBuilder addCast(Actor actor) {
+        this.cast.add(actor);
+        return this;
+    }
+
+}
