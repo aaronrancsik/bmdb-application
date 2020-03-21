89970db198a9aa47d25b5487a80c918a384b73a5
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2020-03-21 03:31:55 +0100

    add config  imports + create AppSpring bean

--- bmdb-application-maven/bmdb-application-app/src/main/java/com/example/bmdb/config/AppConfig.java
+++ bmdb-application-maven/bmdb-application-app/src/main/java/com/example/bmdb/config/AppConfig.java
@@ -1,13 +1,20 @@
 package com.example.bmdb.config;
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import com.example.bmdb.app.AppSpring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.Import;
+
 
 @Configuration
 @ComponentScan("com.example")
+@Import({DomainMockConfig.class, MessageConfig.class })
 public class AppConfig {
+
+    @Bean
+    public AppSpring createAppSpring(){
+        return new AppSpring();
+    }
 
 }
