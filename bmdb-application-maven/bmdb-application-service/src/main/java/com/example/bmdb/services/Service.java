54648f73f6412968078c0e07d14a442e999eba76
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2020-03-21 03:43:49 +0100

    refactor service + get Mocks from DI config

--- bmdb-application-maven/bmdb-application-service/src/main/java/com/example/bmdb/services/Service.java
+++ bmdb-application-maven/bmdb-application-service/src/main/java/com/example/bmdb/services/Service.java
@@ -1,49 +1,60 @@
 package com.example.bmdb.services;
 
 
-import com.example.bmdb.builders.DataBuilder;
 import com.example.bmdb.data.Media;
 import com.example.bmdb.data.Review;
 import com.example.bmdb.data.User;
+//import org.springframework.stereotype.Service;
 
 
-
+import javax.inject.Inject;
+import java.util.Collections;
 import java.util.List;
 
-
+@org.springframework.stereotype.Service
 public class Service {
 
-    DataBuilder builder;
-
-    public Service() {
-        this.builder = new DataBuilder();
-        builder.build();
+    List<User> users;
+    @Inject
+    public void setUsers(List<User> users) {
+        this.users = users;
     }
-
-    public DataBuilder getBuilder(){
-        return builder;
-    }
-
     public void saveUser(User user){
-        this.builder.addUser(user);
+        this.users.add(user);
     }
-
     public User findUser(String email, String password)  {
-        List<User> a = builder.getUsers();
-        return a.stream().filter(u->u.getEmail().equals(email) && u.getPassword().equals(password)).findFirst().get();
+        return users.stream().filter(u->u.getEmail().equals(email) && u.getPassword().equals(password)).findFirst().get();
     }
 
+    List<Media> medias;
+    @Inject
+    public void setMedias(List<Media> medias) {
+        this.medias = medias;
+    }
     public List<Media> findAllMedia() {
-        return this.builder.getMedias();
+        return Collections.unmodifiableList(medias);
     }
 
-    public void saveReviews(Media media, Review review){
+    List<Review> reviews;
+    @Inject
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+    public void saveReview(Media media, Review review, User user){
+        review.setCreator(user);
         review.setMedia(media);
-        this.builder.addReview(review);
+        media.addReview(review);
+        this.reviews.add(review);
+    }
+    public List<Review> findAllReview(Media media) {
+        return Collections.unmodifiableList(reviews);
     }
 
-    public List<Review> findAllReview(Media media) {
-        return this.builder.getReviews(media);
+    public double getAverageRating(Media selected){
+        double sum = selected.getReviews().stream().mapToInt(o->o.getRating().rate).sum();
+        double num = selected.getReviews().stream().count();
+        return  sum / num;
+
     }
 
 
