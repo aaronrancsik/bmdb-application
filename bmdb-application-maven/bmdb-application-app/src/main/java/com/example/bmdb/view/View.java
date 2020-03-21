dcebb469c7d17e23030b9a3136e9c24e7d1fb251
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2020-03-21 03:45:37 +0100

    refactor view with replace to i18n + remove hidden business logic from here

--- bmdb-application-maven/bmdb-application-app/src/main/java/com/example/bmdb/view/View.java
+++ bmdb-application-maven/bmdb-application-app/src/main/java/com/example/bmdb/view/View.java
@@ -7,13 +7,23 @@ import com.example.bmdb.data.Media;
 import com.example.bmdb.data.Rating;
 import com.example.bmdb.data.Review;
 import com.example.bmdb.data.User;
+import org.springframework.stereotype.Component;
 
+
+import javax.inject.Inject;
 import java.util.List;
 import java.util.Scanner;
 
+@Component
 public class View {
+
     private Scanner in;
 
+    I18nMsg i18nMsg;
+    @Inject
+    public void setI18nMsg(I18nMsg i18nMsg) {
+        this.i18nMsg = i18nMsg;
+    }
 
     UserBuilder userBuilder;
     @Inject
@@ -32,81 +42,78 @@ public class View {
     }
 
     public User readUserData()  {
-        UserBuilder userBuilder = new UserBuilder();
 
-        System.out.println("What is your name?");
+        System.out.println(i18nMsg.getMsg("view.question.name"));
         userBuilder.setName(in.nextLine());
 
-        System.out.println("Type your e-mail address:");
+        System.out.println(i18nMsg.getMsg("view.get.email"));
         userBuilder.setEmail(in.nextLine());
 
-        System.out.println("Type a new password:");
+        System.out.println(i18nMsg.getMsg("view.get.newPassword"));
         userBuilder.setPassword(in.nextLine());
 
         return userBuilder.getUser();
     }
 
     public void printWelcomeMessage(User user){
-        System.out.println(String.format("Welcome to the jungle " + user.getName() + "!"));
+        System.out.println(i18nMsg.getMsg("view.welcome",new Object[]{ user.getName() }));
     }
 
     public void printMedias(List<Media> medias){
+        System.out.println(i18nMsg.getMsg("view.medias"));
         for (int i = 0; i < medias.size(); i++){
             System.out.println(i+": "+medias.get(i));
         }
-        System.out.println("Choose an id:");
+        System.out.println(i18nMsg.getMsg("view.get.id"));
     }
 
-    public Media selectMedia(List<Media> medias){
+    public int getMediaNumberFromConsole(){
         String input = in.nextLine();
         int inputNumber = Integer.parseInt(input);
-        int i =-1;
-        for(Media m : medias){
-            if(++i==inputNumber){
-                return m;
-            }
-        }
-        return null;
+        return inputNumber;
     }
 
-    public Review writeReview(){
-        ReviewBuilder reviewBuilder = new ReviewBuilder();
-
-        System.out.println("Write review:");
-        reviewBuilder.setText(in.nextLine());
-
-        System.out.println("Choose a rating! (BAD, AVERAGE, GOOD)");
-        Rating rate= Rating.GOOD;
-        switch (in.nextLine().toLowerCase()){
-            case "bad" :
-                rate=Rating.BAD;
-                break;
-            case "average":
-                rate=Rating.AVERAGE;
-                break;
-            case "good":
-                rate=Rating.GOOD;
-                break;
-        }
-        reviewBuilder.setRating(rate);
+    public Review getReviewFromConsole(){
+        reviewBuilder.setText(getReviewTextFromConsole());
+        reviewBuilder.setRating(getRatingFromConsole());
         return reviewBuilder.getReview();
     }
 
-    public void printReviews(Media selectedMedia){
-        double sum = selectedMedia.getReviews().stream().mapToInt(o->o.getRating().rate).sum();
-        double num = selectedMedia.getReviews().stream().count();
-        double average = sum/num;
-        System.out.println("Average of reviews: " + average);
+    private String getReviewTextFromConsole() {
+        System.out.println(i18nMsg.getMsg("view.get.review"));
+        return in.nextLine();
     }
 
-    public boolean questionYesOrNo(String message) {
+    private Rating getRatingFromConsole() {
+        Rating rate = null;
+        while (rate == null){
+            System.out.println(i18nMsg.getMsg("view.get.rating"));
+            String choosedRate = in.nextLine().toLowerCase();
+            if(choosedRate.equals(i18nMsg.getMsg("view.rate.bad"))) {
+                rate=Rating.BAD;
+            }
+            else if(choosedRate.equals(i18nMsg.getMsg("view.rate.average"))){
+                rate=Rating.AVERAGE;
+            }
+            else if(choosedRate.equals(i18nMsg.getMsg("view.rate.good"))){
+                rate=Rating.GOOD;
+            }
+        }
+        return rate;
+    }
+
+    public void averageReviewsToConsole(double average){
+        System.out.println(i18nMsg.getMsg("view.averageReview", average));
+    }
+
+    public boolean anotherReviewFromConsole() {
         while (true){
-            System.out.println(message + " [yes/no]");
-            switch (in.nextLine()) {
-                case "yes":
-                    return true;
-                case "no":
-                    return false;
+            System.out.println(i18nMsg.getMsg("view.question.anotherReview"));
+            String yesOrNo = in.nextLine();
+            if(yesOrNo.equals(i18nMsg.getMsg("view.yes"))){
+                return true;
+            }else if(yesOrNo.equals(i18nMsg.getMsg("view.no"))){
+                return false;
             }
         }
     }
