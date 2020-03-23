1335f00736b42f9a581a165a7cc06529dee5473b
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2020-03-23 20:25:18 +0100

    refactoring to use new distributed service classes

--- bmdb-application-maven/bmdb-application-app/src/main/java/com/example/bmdb/app/AppSpring.java
+++ bmdb-application-maven/bmdb-application-app/src/main/java/com/example/bmdb/app/AppSpring.java
@@ -1,81 +1,119 @@
 package com.example.bmdb.app;
 
 
-import com.example.bmdb.services.Service;
+import com.example.bmdb.models.Actor;
+import com.example.bmdb.models.Media;
+import com.example.bmdb.models.Review;
+import com.example.bmdb.models.User;
 import com.example.bmdb.config.AppConfig;
-import com.example.bmdb.data.Media;
-import com.example.bmdb.data.Review;
-import com.example.bmdb.data.User;
+import com.example.bmdb.services.*;
 import com.example.bmdb.view.View;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import javax.inject.Inject;
-import java.util.List;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 
 public class AppSpring {
 
     private static Logger log = LoggerFactory.getLogger(AppSpring.class);
 
-    private Service service;
-    private View view;
-
+    private UserService userService;
     @Inject
-    public void setService(Service service) {
-        log.info("setGeneralService");
-        this.service = service;
+    public void setUserService(UserService userService) {
+        this.userService = userService;
     }
 
+    private MediaService mediaService;
+    @Inject
+    public void setMediaService(MediaService mediaService) {
+        this.mediaService = mediaService;
+    }
+
+    private ActorService actorService;
+    @Inject
+    public void setActorService(ActorService actorService) {
+        this.actorService = actorService;
+    }
+
+    private ReviewService reviewService;
+    @Inject
+    public void setReviewService(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    private MainService mainService;
+    @Inject
+    public void setMainService(MainService mainService) {
+        this.mainService = mainService;
+    }
+
+    private View view;
     @Inject
     public void setView(View view) {
         log.info("setView");
         this.view = view;
     }
 
+    @Inject
+    private List<Media> mediaList;
+
+    @Inject
+    private List<Actor> actorList;
+
+    @Inject
+    private List<Review> reviewList;
+
+    @Inject
+    private List<User> userList;
 
     private void launch() {
+
+        FillMocksToDB();
+
         log.info("launch");
         User user = createUser();
-        List<Media> medias = service.findAllMedia();
         do{
-            view.printMedias(medias);
-            Media selected = selectMedia(medias);
-            doReviews(user, selected);
+            view.printMedias(mediaService.findAll());
+            long selectedMediaId = selectMediaId();
+            mainService.doReview(user.getName(),user.getPassword(), selectedMediaId,view.getReviewTextFromConsole(),view.getRatingFromConsole());
         }while (view.anotherReviewFromConsole());
 
-        view.printMedias(medias);
-        Media selected = selectMedia(medias);
-        averageReviews(selected);
+        view.printMedias(mediaService.findAll());
+        long selectedMediaId = selectMediaId();
+        averageReviews(selectedMediaId);
+    }
+
+    private void FillMocksToDB() {
+        actorService.saveAll(actorList);
+        mediaService.saveAll(mediaList);
+        userService.saveAll(userList);
+        reviewService.saveAll(reviewList);
     }
 
     private User createUser(){
+        view.printRegistration();
         log.info("createUser");
         User user = this.view.readUserData();
-        service.saveUser(user);
-        this.view.printWelcomeMessage(user);
+        userService.saveUser(user);
+        view.printWelcomeMessage(user);
         return user;
     }
 
-    private Media selectMedia(List<Media> medias){
+    private long selectMediaId(){
         log.info("selectMedia");
-        return medias.get(view.getMediaNumberFromConsole());
+        return view.getIdFromConsole();
     }
 
-
-    private void doReviews(User user, Media media){
-        log.info("doReviews");
-        Review review = view.getReviewFromConsole();
-        service.saveReview(media,review,user);
-
-    }
-
-    private void averageReviews(Media selected){
+    private void averageReviews(long selected){
         log.info("averageReviews");
-        view.averageReviewsToConsole(service.getAverageRating(selected));
+        view.averageReviewsToConsole(mediaService.getAverageRating(selected));
     }
 
     public static void main(String[] args) {
